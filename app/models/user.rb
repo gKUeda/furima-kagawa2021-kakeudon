7c81805c887f0238a104b308902cd4b6e9723cda
babachande <mamayakan@gmail.com>
2021-10-05 16:39:30 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
+         validates :encrypted_password,:password,:password_confirmation,length:{minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
+         validates :nickname, presence: true
+         validates :lastname, presence:true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+         validates :firstname, presence:true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+         validates :lastname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+         validates :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+         validates :birthday, presence: true     
 end
+
+
