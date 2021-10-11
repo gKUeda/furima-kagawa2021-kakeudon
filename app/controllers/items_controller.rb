3bb29a1c732f8915cf61165e380cf9bc800ee96e
Tamagorou <sattcchn.0426@gmail.com>
2021-10-11 19:59:28 +0900

    items_controller.rbにnewアクションとcreateアクションを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+  end
+end
+
+private
+def item_params
+params.require(:item).pemit(:item_name, :info, :category_id, :salesstatus_id, :shippingfees_id, :prefecture_id, :delivery_id, :price).merge(user_id: current_user.id)
 end
