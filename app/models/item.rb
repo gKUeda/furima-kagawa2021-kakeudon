cb08495c38af8140034c527d4bf1decd327b8db0
Tamagorou <sattcchn.0426@gmail.com>
2021-10-13 18:07:20 +0900

    モデルの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,22 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :order
+  has_one_attached :image
 
+  validates :item_name, presence: true
+  validates :info, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :salesstatus_id, numericality: { other_than: 1 }
+  validates :shippingfees_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :delivery_id, numericality: { other_than: 1 }
+  validates :price, presence: true
+  validates :image, presence: true
+
+  def was_attached?
+    self.image.attached?
+  end
 end
+
+
 
