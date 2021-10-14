48bef7ce288c32b7631df5c2c53a7843a225dccd
Tamagorou <sattcchn.0426@gmail.com>
2021-10-14 19:37:39 +0900

    active_hash導入（プルダウン表示）

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,19 @@ class Item < ApplicationRecord
   has_one :order
   has_one_attached :image
 
+  belongs_to :category
+  belongs_to :salesstatus
+  belongs_to :shippingfees
+  belongs_to :prefecture
+  belongs_to :delivery
+
   validates :item_name, presence: true
   validates :info, presence: true
-  validates :category_id, numericality: { other_than: 1 }
-  validates :salesstatus_id, numericality: { other_than: 1 }
-  validates :shippingfees_id, numericality: { other_than: 1 }
-  validates :prefecture_id, numericality: { other_than: 1 }
-  validates :delivery_id, numericality: { other_than: 1 }
+  validates :category_id, numericality: { other_than: 1,}
+  validates :salesstatus_id, numericality: { other_than: 1, }
+  validates :shippingfees_id, numericality: { other_than: 1, }
+  validates :prefecture_id, numericality: { other_than: 0, }
+  validates :delivery_id, numericality: { other_than: 1, }
   validates :price, presence: true
   validates :image, presence: true
 
