656852718eb74d17b7b450120e2945cc1da90f74
Tamagorou <sattcchn.0426@gmail.com>
2021-10-15 17:43:53 +0900

    active_storage導入（画像の保存）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
   def index
-    
   end
 
   def new
@@ -15,5 +14,5 @@ end
 
 private
 def item_params
-params.require(:item).pemit(:item_name, :info, :category_id, :salesstatus_id, :shippingfees_id, :prefecture_id, :delivery_id, :price).merge(user_id: current_user.id)
+params.require(:item).permit(:item_name, :info, :category_id, :salesstatus_id, :shippingfees_id, :prefecture_id, :delivery_id, :price, :image).merge(user_id: current_user.id)
 end
