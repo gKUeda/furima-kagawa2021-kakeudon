397d30653b3a8af60d06e22734276184e7d32277
Tamagorou <sattcchn.0426@gmail.com>
2021-10-15 19:05:42 +0900

    バリデーションの追記（値の制限・エラーメッセージ）

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,13 +11,13 @@ class Item < ApplicationRecord
 
   validates :item_name, presence: true
   validates :info, presence: true
-  validates :category_id, numericality: { other_than: 1,}
-  validates :salesstatus_id, numericality: { other_than: 1, }
-  validates :shippingfees_id, numericality: { other_than: 1, }
-  validates :prefecture_id, numericality: { other_than: 0, }
-  validates :delivery_id, numericality: { other_than: 1, }
-  validates :price, presence: true
-  validates :image, presence: true
+  validates :category_id, numericality: { other_than: 1, message:"Category can't be blank"}
+  validates :salesstatus_id, numericality: { other_than: 1, message:"Sales status can't be blank"}
+  validates :shippingfees_id, numericality: { other_than: 1, message:"Shipping fee status can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 0, message:"Prefecture can't be blank"}
+  validates :delivery_id, numericality: {{ in: 300..9,999,999},other_than: 1, message:"Scheduled delivery can't be blank"}
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message:"Price is invalid. Input half-width characters", "Price is out of setting range" }
+  validates :image, presence: true, unless: :was_attached?
 
   def was_attached?
     self.image.attached?
