544cac0ec9c97395a3f1b1c7922c8203c8689050
Tamagorou <sattcchn.0426@gmail.com>
2021-10-16 22:16:36 +0900

    with_optionsを使った書き方に変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :order
   has_one_attached :image
-  
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :salesstatus
@@ -10,20 +10,15 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :delivery
 
-  validates :item_name, presence: true
-  validates :info, presence: true
-  validates :category_id, numericality: { other_than: 1, message:"Category can't be blank"}
-  validates :salesstatus_id, numericality: { other_than: 1, message:"Sales status can't be blank"}
-  validates :shippingfees_id, numericality: { other_than: 1, message:"Shipping fee status can't be blank"}
-  validates :prefecture_id, numericality: { other_than: 0, message:"Prefecture can't be blank"}
-  validates :delivery_id, numericality: {in: 300..9999999,other_than: 1, message:"Scheduled delivery can't be blank"}
-  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message:"Price is invalid. Input half-width characters" "Price is out of setting range" }
-  validates :image, presence: true, unless: :was_attached?
-
-  def was_attached?
-    self.image.attached?
+  with_options presence: true do
+  validates :image
+  validates :item_name
+  validates :info
+  validates :price,format: { with: /\A[0-9]+\z/ , message: "is invalid. Input half-width characters" }, inclusion: { in: 300..9999999, message:"is out of setting range"}
   end
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :salesstatus_id, numericality: { other_than: 1, message:"can't be blank"}
+  validates :shippingfees_id, numericality: { other_than: 1, message:"can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 0, message:"can't be blank"}
+  validates :delivery_id, numericality: {other_than: 1,message:"can't be blank" }
 end
-
-
-
