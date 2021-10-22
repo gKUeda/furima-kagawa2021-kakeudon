728ebe2fbcb8911221c08db1eb615cbc74caf5e5
RunakoMasuda <88620934+RunakoMasuda@users.noreply.github.com>
2021-10-22 14:19:14 +0900

    orderコントローラーにorder_addressセット

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,12 @@
 class OrdersController < ApplicationController
+before_action :authenticate_user!, only: :index
 
 def index
   @item = Item.find(params[:item_id])
+  @order_address =OrderAddress.new
+end
+
+def create
 end
 
 end
