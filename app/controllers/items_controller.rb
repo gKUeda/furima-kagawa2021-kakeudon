4c1275cfa07ad17aa5e287556499f8f4e6892567
Tamagorou <sattcchn.0426@gmail.com>
2021-10-23 15:47:57 +0900

    コントローラーにアクションを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,6 +34,15 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item= Item.find(params[:id])
+    if item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
 
   private
 
