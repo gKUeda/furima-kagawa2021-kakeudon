52a5e829654eecb70621159369685f25ac2f26e9
RunakoMasuda <88620934+RunakoMasuda@users.noreply.github.com>
2021-10-24 00:09:28 +0900

    フォームオブジェクトの実装_モデル

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,18 +1,15 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :order_id, :user_id, :token
-
+  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :item_id, :user_id, :token
+#binding.pry
   with_options presence: true do
-    validates :postal_code, format: {with: /\A\d{3}[-]\d{2}\z/}
+    validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/}
     validates :city
     validates :addresses
     validates :phone_number, numericality: {with: /\A\d{10,11}\z/, message: "is invalid. Include hyphen(-)"}
-
-    validates :order_id
+    validates :item_id
     validates :user_id
-
     validates :token
-
   end
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
 
@@ -21,7 +18,7 @@ class OrderAddress
     order = Order.create(item: item_id, user_id: user_id)
     # 住所を保存する
     # order_idには、変数orderのidと指定する
-    Delivery.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
   end
 
 end
