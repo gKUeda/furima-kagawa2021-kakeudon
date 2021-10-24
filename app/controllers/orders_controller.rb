c3ab342106a8a0ff35c618bdc5b54688432a90df
RunakoMasuda <88620934+RunakoMasuda@users.noreply.github.com>
2021-10-24 15:38:33 +0900

    商品購入機能の実装確認

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,20 +4,16 @@ before_action :authenticate_user!, only: :index
 def index
   @item = Item.find(params[:item_id])
   @order_address = OrderAddress.new
-  binding.pry
 end
 
 def create
   @item = Item.find(params[:item_id])
   @order_address = OrderAddress.new(order_params)
-  binding.pry
   if @order_address.valid?
-    binding.pry
     pay_item
     @order_address.save
     redirect_to root_path
   else
-    binding.pry
     render :index
   end
 end
@@ -25,17 +21,15 @@ end
 private
 
   def order_params
-    binding.pry
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
-    binding.pry
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
     Payjp::Charge.create(
-      amount: order_params[:price],  # 商品の値段
-      card: order_params[:token],    # カードトークン
-      currency: 'jpy'                 # 通貨の種類（日本円）
+      amount: @item.price,  
+      card: order_params[:token],    
+      currency: 'jpy'                 
     )
   end
 
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -12,12 +12,9 @@ class OrderAddress
     validates :token
   end
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
-
+  
   def save
-    # 購入情報を保存し、変数orderに代入する
-    order = Order.create(item: item_id, user_id: user_id)
-    # 住所を保存する
-    # order_idには、変数orderのidと指定する
+    order = Order.create(item_id: item_id, user_id: user_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
   end
 
