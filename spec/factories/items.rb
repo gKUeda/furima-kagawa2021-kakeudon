96424ec003c7a1b14a1d741a038017422799691c
RunakoMasuda <88620934+RunakoMasuda@users.noreply.github.com>
2021-10-26 15:34:40 +0900

    itemモデルのテスト修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :item do
-    image         { Faker::Lorem.sentence }
     item_name     { Faker::Lorem.sentence }
     info          { Faker::Lorem.sentence }
     category_id        { 2 }
@@ -10,5 +9,10 @@ FactoryBot.define do
     delivery_id        { 2 }
     price { 2000 }
     association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/star.png'), filename: 'star.png')
+    end
+
   end
 end
